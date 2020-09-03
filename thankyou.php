4c0a17b866cccbb99a7bc9219f2e78b3cdc800e1
offcorse <57239657+offcorse@users.noreply.github.com>
2020-09-03 15:41:23 -0600

    Initial commit

---
+++ thankyou.php
@@ -0,0 +1,134 @@
+<!----------------------------------------------------------------------------------------------------------------
+--
+
+#Original Author: Scott Pinkerton   
+#Date Created: 09/03/2020                                         
+#Version: 1.0                                                    
+#Date Last Modified:09/03/2020                                
+#Modified by: Scott                                          
+#Modification log: Initial release 1.0
+
+ --
+------------------------------------------------------------------------------------------------------------------>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>rscottpinkerton</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cabin:700">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/Contact-Form-Clean.css">
+    <link rel="stylesheet" href="assets/css/Footer-Dark.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/css/lightbox.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/css/swiper.min.css">
+    <link rel="stylesheet" href="assets/css/Lightbox-Gallery.css">
+    <link rel="stylesheet" href="assets/css/Simple-Slider.css">
+<link rel="icon" 
+      type="image/png" 
+      href="assets/img/small_sp.png">
+</head>
+
+<body id="page-top">
+    <nav class="navbar navbar-light navbar-expand-md navbar navbar-expand-lg" id="mainNav">
+        <div class="container"><a class="navbar-brand js-scroll-trigger" href="index.html" style="font-family: Lora, serif;">rscottpinkerton</a><button data-toggle="collapse" class="navbar-toggler navbar-toggler-right" data-target="#navbarResponsive" type="button" aria-controls="navbarResponsive"
+                aria-expanded="false" aria-label="Toggle navigation" value="Menu"><i class="fa fa-bars"></i></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="nav navbar-nav ml-auto">
+                    <li class="nav-item nav-link js-scroll-trigger" role="presentation"><a class="nav-link active js-scroll-trigger" href="index.html">Home</a></li>
+                    <li class="nav-item nav-link js-scroll-trigger" role="presentation"><a class="nav-link active js-scroll-trigger" href="#about">Pro About</a></li>
+                    <li class="nav-item nav-link js-scroll-trigger" role="presentation"></li>
+                    <li class="nav-item nav-link js-scroll-trigger" role="presentation"><a class="nav-link js-scroll-trigger" href="Pers%20About.html">Pers About</a></li>
+                    <li class="nav-item nav-link js-scroll-trigger" role="presentation"><a class="nav-link js-scroll-trigger" href="portfolio2.html">Portfolio</a></li>
+                    <li class="nav-item nav-link js-scroll-trigger" role="presentation"><a class="nav-link js-scroll-trigger" href="resume.html">Resume</a></li>
+                    <li class="nav-item nav-link js-scroll-trigger" role="presentation"><a class="nav-link js-scroll-trigger" href="index.html#contact">contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!--shoot stars canvas-->
+    <canvas id="canvas1" width="1500" height="100px";></canvas>
+    
+    <!--The shooting star javascript works when loaded here just below the canvas-->
+    <script src="assets/js/shootingstars.js"></script>
+<?php
+    
+    $visitor_name = filter_input(INPUT_POST, 'name');
+    $visitor_email = filter_input(INPUT_POST, 'email');
+    $visitor_msg = filter_input(INPUT_POST, 'message');
+    
+    /* echo "Fields: " . $visitor_name . $visitor_email . $visitor_msg;  */
+    
+    // Validate inputs
+    if ($visitor_name == null || $visitor_email == null ||
+        $visitor_msg == null) {
+        $error = "Invalid input data. Check all fields and try again.";
+        /* include('error.php'); */
+        echo "Form Data Error: " . $error; 
+        exit();
+        } else {
+            $dsn = 'mysql:host=localhost;dbname=portfoliocontact';
+            $username = 'my_user';
+            $password = 'Pa$$w0rd';
+
+            try {
+                $db = new PDO($dsn, $username, $password);
+
+            } catch (PDOException $e) {
+                $error_message = $e->getMessage();
+                /* include('database_error.php'); */
+                echo "DB Error: " . $error_message; 
+                exit();
+            }
+
+            // Add the product to the database  
+            $query = 'INSERT INTO contacts
+                         (contactName, contactEmail, contactMsg, contactDate, employeeID)
+                      VALUES
+                         (:contactName, :contactEmail, :contactMsg, NOW(), 1)';
+            $statement = $db->prepare($query);
+            $statement->bindValue(':contactName', $visitor_name);
+            $statement->bindValue(':contactEmail', $visitor_email);
+            $statement->bindValue(':contactMsg', $visitor_msg);
+            $statement->execute();
+            $statement->closeCursor();
+            /* echo "Fields: " . $visitor_name . $visitor_email . $visitor_msg; */
+
+}
+
+?>
+<section>
+  <h2>Thank you, <?php echo $visitor_name; ?>, for contacting me! I will get back to you shortly.</h2>
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+
+  <p>&nbsp;</p>
+</section>
+    <div class="footer-dark">
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col item social"><a href="https://www.linkedin.com/in/rscottpinkerton/" target=_blank><i class="icon ion-social-linkedin"></i></a>
+                        <a href="https://github.com/offcorse/" target="_blank"><i class="icon ion-social-github"></i></a></div>
+                </div>
+                <p class="copyright">Company Name © 2017</p>
+            </div>
+        </footer>
+    </div>
+    <section class="navbar navbar-expand-lg fixed-top"></section>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/bs-init.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+    <script src="assets/js/grayscale.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/js/lightbox.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/js/swiper.jquery.min.js"></script>
+    <script src="assets/js/Simple-Slider.js"></script>
+</body>
+
+</html>
+
