bce3472f7d7ff844823729894e3cb9f2f2379e3d
spinkerton <spinkerton@FA20ServerA03.SWDV.local>
2020-09-11 16:56:30 -0600

    Added background image to header

--- admin.php
+++ admin.php
@@ -33,6 +33,7 @@
 </head>
 
 <body id="page-top">
+    
     <nav class="navbar navbar-light navbar-expand-md navbar navbar-expand-lg" id="mainNav">
         <div class="container"><a class="navbar-brand js-scroll-trigger" href="index.html" style="font-family: Lora, serif;">rscottpinkerton</a><button data-toggle="collapse" class="navbar-toggler navbar-toggler-right" data-target="#navbarResponsive" type="button" aria-controls="navbarResponsive"
                 aria-expanded="false" aria-label="Toggle navigation" value="Menu"><i class="fa fa-bars"></i></button>
